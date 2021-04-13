d865aa82eebf4e0984a68e466e8a300f9b611744
eduBallesta <eduardo_ballesta81@hotmail.com>
2021-04-13 08:18:03 +0200

    Version 1.0

---
+++ 01-Socket_Server/models/server.js
@@ -0,0 +1,46 @@
+const express   = require('express');
+const http      = require('http');
+const socketio  = require('socket.io');
+const path      = require('path');
+const Sockets   = require('./sockets');
+
+class Server {
+
+    constructor() {
+
+        this.app  = express();
+        this.port = process.env.PORT;
+
+        // Http server
+        this.server = http.createServer( this.app );
+
+        // Configuraciones de socket
+        this.io = socketio ( this.server, { /* Configuraciones */ });
+    }
+
+    middelwares() {
+        this.app.use( express.static(  path.resolve( __dirname, '../public')))
+    }
+
+    configurarSockets() {
+        new Sockets(this.io);
+    }
+
+    execute() {
+        // Inicializar Middleware
+        this.middelwares();
+
+        this.configurarSockets();
+
+        // Inicializar Server
+        this.server.listen(this.port, () => {
+            console.log('Server corriendo');
+        });
+    }
+
+
+}
+
+
+
+module.exports = Server;
